3f4e946f70fa4b9cfb7a177e5eb1885a43ae27bc
Keith Bodger <keithbodger@icloud.com>
2026-06-19 15:56:55 +0100

    Update API_URL in config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.IBD_CONFIG = {
   SERVICE_NAME: "Your IBD Service",
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbwcl0UA0I1Yhd_WmtvgXTLj8ZB_yYLQrRAUN2Yxslv267D58H2R_fQsJRdAHyCHSbe0/exec",
   DASHBOARD_TITLE: "IBD Standards Self-Assessment Dashboard"
 };
